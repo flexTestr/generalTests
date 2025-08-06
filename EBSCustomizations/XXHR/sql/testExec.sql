af32d99346d6dac9c2b3793e79f01157509b7bcc
flexTestr <200045432+flexTestr@users.noreply.github.com>
2025-08-06 22:51:26 +0530

    Update testExec.sql

--- EBSCustomizations/XXHR/sql/testExec.sql
+++ EBSCustomizations/XXHR/sql/testExec.sql
@@ -1,5 +1,5 @@
 DECLARE v_result NUMBER;
 BEGIN 
-EXECUTE IMMEDIATE 'SELECT 1 FROM dual' INTO v_result;
+EXEC 'SELECT 1 FROM dual' INTO v_result;
 DBMS_OUTPUT.PUT_LINE('Result: ' || v_result);
 END;
