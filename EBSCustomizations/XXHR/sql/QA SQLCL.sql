7810e30211acd5d1b1e61827ea5272f70ed589d3
flexTestr <200045432+flexTestr@users.noreply.github.com>
2026-05-15 21:03:27 +0530

    Create QA SQLCL.sql

---
+++ EBSCustomizations/XXHR/sql/QA SQLCL.sql
@@ -0,0 +1,10 @@
+SET SERVEROUTPUT ON
+
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('Script started...');
+    DBMS_OUTPUT.PUT_LINE('DB Time: ' || TO_CHAR(SYSDATE, 'YYYY-MM-DD HH24:MI:SS'));
+    DBMS_OUTPUT.PUT_LINE('Script completed successfully, cheersssss..');
+END;
+/
+
+EXIT
